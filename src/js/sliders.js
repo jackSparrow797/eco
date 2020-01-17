04c564da757e8b3cc19cc8257f7eb9b97aefcea2
Медведев Евгений <medvedev@adv.ru>
2020-01-17 17:07:38 +0400

    add news-style

--- src/js/sliders.js
+++ src/js/sliders.js
@@ -39,7 +39,8 @@ $(document).ready(function () {
         },{
             breakpoint: 760,
             settings: {
-                slidesToShow: 1
+                slidesToShow: 1,
+                dots: true
             }
         },
         ]
